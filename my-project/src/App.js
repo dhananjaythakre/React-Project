d2055826d051fb88fdf1d08fc282086dbdde2702
dhananjaythakre <amolthakre26@gmail.com>
2022-03-14 22:26:12 +0530

    Initial-project

---
+++ my-project/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="topHeader">Ecomm-Product</div>
+      </header>
+      <main>
+        <div className="leftSide">Project List</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
