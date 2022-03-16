cd7441084041d4cc79544e57cdfbebf06798f258
dhananjaythakre <amolthakre26@gmail.com>
2022-03-16 22:48:53 +0530

    16-date Task

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,3 +1,6 @@
+import data from "./data";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
@@ -5,7 +8,23 @@ function App() {
         <div className="topHeader">Ecomm-Product</div>
       </header>
       <main>
-        <div className="leftSide">Project List</div>
+        <h1>Featured Product</h1>
+        <div className="products">
+        {
+          data.products.map(product =>(
+          <div className="product" key={product.slug}>
+            <img src={product.image} alt={product.name} />
+            <p>
+              {product.name}              
+            </p>
+            <p>
+            {product.price}
+            </p>
+            <Button>Click Here</Button>
+
+          </div>))
+        }
+        </div>
       </main>
     </div>
   );
